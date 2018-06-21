3f660dd1bdd6b7e8b98123314e6129da1f0b3733
fenrisl <daniel.deutsch25@gmail.com>
2018-06-21 19:04:38 +0200

    test

--- client/containers/Chat.js
+++ client/containers/Chat.js
@@ -1,5 +1,6 @@
 import React, {Component} from 'react'
 import {View, Text, StyleSheet, ScrollView, Button} from 'react-native'
+import GameOfChoices from '../components/GameOfChoices.json'
 
 class Chat extends Component {
     constructor()
@@ -8,20 +9,25 @@ class Chat extends Component {
 
         this.state = {
             valueArray: [],
-            Right: false
+            Playing: false,
+            ButtonStateHolder1: false,
+            ButtonStateHolder2: false,
+            ButtonStateHolder3: false,
+            ButtonStateHolder4: false
         }
-
         this.index = 0
     }
     state = {}
 
-    addMore = () => {
+    addMore1 = () => {
         let newlyAddedValue = {
             index: this.index
         }
 
+        this.setState({ButtonStateHolder1: true})
+
         this.setState({
-            disabled: true,
+            // disabled: true,
             valueArray: [
                 ...this.state.valueArray,
                 newlyAddedValue
@@ -31,6 +37,94 @@ class Chat extends Component {
         })
     }
 
+    addMore2 = () => {
+        let newlyAddedValue = {
+            index: this.index
+        }
+
+        this.setState({ButtonStateHolder2: true})
+
+        this.setState({
+            // disabled: true,
+            valueArray: [
+                ...this.state.valueArray,
+                newlyAddedValue
+            ]
+        }, () => {
+            this.index = this.index++
+        })
+    }
+
+    addMore3 = () => {
+        let newlyAddedValue = {
+            index: this.index
+        }
+
+        this.setState({ButtonStateHolder3: true})
+
+        this.setState({
+            // disabled: true,
+            valueArray: [
+                ...this.state.valueArray,
+                newlyAddedValue
+            ]
+        }, () => {
+            this.index = this.index++
+        })
+    }
+
+    addMore4 = () => {
+        let newlyAddedValue = {
+            index: this.index
+        }
+
+        this.setState({ButtonStateHolder4: true})
+
+        this.setState({
+            // disabled: true,
+            valueArray: [
+                ...this.state.valueArray,
+                newlyAddedValue
+            ]
+        }, () => {
+            this.index = this.index++
+        })
+    }
+
+    blop = 0
+    Playing = () => {
+        if (blop === 1) {
+            
+                <View>
+
+                    <Button
+                        disabled={this.state.ButtonStateHolder1}
+                        onPress={this.addMore1}
+                        title={GameOfChoices['a']}
+                        color="#841584"/>
+
+                    <Button
+                        disabled={this.state.ButtonStateHolder2}
+                        onPress={this.addMore2}
+                        title={GameOfChoices['ab']}
+                        color="#841584"/>
+
+                    <Button
+                        disabled={this.state.ButtonStateHolder3}
+                        onPress={this.addMore3}
+                        title={GameOfChoices['abc']}
+                        color="#841584"/>
+
+                    <Button
+                        disabled={this.state.ButtonStateHolder4}
+                        onPress={this.addMore4}
+                        title={GameOfChoices['abcd']}
+                        color="#841584"/>
+                </View>
+            
+        }
+    }
+
     render() {
         {
 
@@ -38,18 +132,59 @@ class Chat extends Component {
                 .state
                 .valueArray
                 .map((item, key) => {
-                    if ((key % 2) == 3 % 1) {
+
+                    if ((key) === 0) {
                         return (
-                            <View key={key} style ={styles.container}>
-                                <Text style ={styles.text}>Row {item.index}</Text>
+                            <View key={key}>
+                                <View style ={styles.containerChat}>
+                                    <Text>indice</Text>
+                                </View>
+                                <View style ={styles.containerChat}>
+                                    <Text style ={styles.textRight}>reponse</Text>
+                                </View>
                             </View>
                         )
-                    } else {
+
+                    }
+
+                    if ((key) === 1) {
                         return (
-                            <View key={key} style ={styles.container}>
-                                <Text>Row {item.index}</Text>
+                            <View key={key}>
+                                <View style ={styles.containerChat}>
+                                    <Text>indice2</Text>
+                                </View>
+                                <View style ={styles.containerChat}>
+                                    <Text style ={styles.textRight}>reponse2</Text>
+                                </View>
                             </View>
                         )
+
+                    }
+                    if ((key) === 2) {
+                        return (
+                            <View key={key}>
+                                <View style ={styles.containerChat}>
+                                    <Text>indice3</Text>
+                                </View>
+                                <View style ={styles.containerChat}>
+                                    <Text style ={styles.textRight}>reponse3</Text>
+                                </View>
+                            </View>
+                        )
+
+                    }
+                    if ((key) === 3) {
+                        return (
+                            <View key={key}>
+                                <View style ={styles.containerChat}>
+                                    <Text>indice4</Text>
+                                </View>
+                                <View style ={styles.containerChat}>
+                                    <Text style ={styles.textRight}>reponse4</Text>
+                                </View>
+                            </View>
+                        )
+
                     }
                 })
                 return (
@@ -67,14 +202,7 @@ class Chat extends Component {
                             </View>
                         </ScrollView>
                         <View>
-                            <Button
-                                style
-                                ={styles.container}
-                                onPress={this.addMore}
-                                title='Blop'
-                                color="#841584"
-                                bottom="0"
-                                flex="1"/>
+                            {this.Playing}
                         </View>
                     </View>
                 )
@@ -82,13 +210,13 @@ class Chat extends Component {
     }
 }
 const styles = StyleSheet.create({
-    text: {
+    textRight: {
         fontSize: 19,
         fontWeight: 'bold',
         textAlign: 'right',
         flex: 1
     },
-    container: {
+    containerChat: {
         flex: 1
     }
 })
